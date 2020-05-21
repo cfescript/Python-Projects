543d86004ce2673d82f44b4684b5e05b805ece78
cfescript <cfescript.exe@gmail.com>
2020-05-21 11:56:17 -0400

    Create Child Classes

---
+++ Small Projects/ChildClasses.py
@@ -0,0 +1,17 @@
+
+
+
+
+class Dog:
+    hasTail = True
+    hasFur = True
+
+class Labradoor(Dog):
+    color = "yellow, brown, or black"
+    pattern = "none"
+    size = "medium-large"
+
+class GermanShepard(Dog):
+    color = "brown and black"
+    pattern = "top and bottom"
+    size = "large"
